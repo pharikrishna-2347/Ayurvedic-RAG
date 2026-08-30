d1e63bdf57bb51ee9af649365a316e08d8fce1f6
pharikrishna-2347 <pharikrishna230407@gmail.com>
2026-08-31 03:26:53 +0530

    Remove unused LLM imports

--- rag/llm.py
+++ rag/llm.py
@@ -1,7 +1,5 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
 load_dotenv()
 
 from langchain_groq import ChatGroq
